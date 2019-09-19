541afbaa7cc91a584270c278bef8c64ea04dda77
Sisco Puig <siscopuigdev@gmail.com>
2019-09-19 19:39:25 +0200

    Added django-heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,7 +163,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 SOCIALACCOUNT_FORMS = {'disconnect': 'users.forms.CustomSocialDisconnectForm'}
 
-
+django_heroku.settings(locals())
 
 
 # ACCOUNT_ADAPTER = 'users.adapter.NoNewUsersAccountAdapter'
