94ad42a2ecac9f95f64e7edd0d6b67bdc2b486dc
Sisco Puig <siscopuigdev@gmail.com>
2019-09-19 17:51:22 +0200

    Added domain in ALLOWED_HOSTS

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k9qw@%hr9b3xjrwhldu!n$^ah9_9ukuhasw$hj_*-8g(cvfx35'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-allauth-demo.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
