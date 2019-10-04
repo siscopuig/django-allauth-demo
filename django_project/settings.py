a10b6aeca6a857b43aad363ab38c4c0f315736e3
Sisco Puig <siscopuig@Siscos-MacBookOld.local>
2019-10-04 12:24:11 +0100

    Removed lab dir

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django.contrib.sites', # New
+    'django.contrib.sites',
 
     'allauth',
     'allauth.account',
